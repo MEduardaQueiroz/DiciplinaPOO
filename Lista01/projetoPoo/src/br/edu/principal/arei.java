a4c352b53ddc917a2acfbb6a88fbe6c37e6a294a
MEduardaQueiroz <142404475+MEduardaQueiroz@users.noreply.github.com>
2023-09-26 13:40:20 -0300

    Add files via upload

---
+++ Lista01/projetoPoo/src/br/edu/principal/arei.java
@@ -0,0 +1,7 @@
+package br.edu.principal;
+
+public class arei {
+	public static void main(String[] args) {
+		String[] mes = {"Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"};
+	}
+}
