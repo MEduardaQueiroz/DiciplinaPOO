40eb9deb329b3310e1fbed669ed452dfc90ecbde
MEduardaQueiroz <142404475+MEduardaQueiroz@users.noreply.github.com>
2023-09-26 13:53:11 -0300

    Add files via upload

---
+++ Lista01/Q23R/src/br/edu/principal/Principal.java
@@ -0,0 +1,25 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double num , frac , arred ;
+		int i;
+		
+		System.out.println("Receba um numero real :  ");
+		num = sc.nextDouble();
+		i = (int)num;
+		frac = num - i;
+		arred =Math.round(num);
+		
+		System.out.println("A parte inteira desse número :  " + i);
+		System.out.println("A parte fracionaria desse número : "+frac);
+		System.out.println("O arredondamento desse numero : "+ arred);
+		
+
+	}
+
+}
